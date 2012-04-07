eac8428c226ad49a4061b792d6d5be90eb85df09
Rollen Gomes <avelliance@gmail.com>
2012-04-07 22:27:07 +0800

    add appfile to bootstrap a sample application

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+require('./nervecenter.js');
+
+var port = process.env.PORT || 8888;
+http.createServer(function onRequestReceived(request, response){
+  console.log('attempting to route to ' + request.url);
+  Nervebuilder.createApplication(request, response, require('fs')).executeRequest();
+}).listen(port);
