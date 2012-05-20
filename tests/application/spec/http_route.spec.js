9985626be31634785988f88e3f1d9d1299e4f60e
Rollen Gomes <avelliance@gmail.com>
2012-05-20 14:29:10 +0800

    add test for falsy path matching in httproute

--- tests/application/spec/http_route.spec.js
+++ tests/application/spec/http_route.spec.js
@@ -12,10 +12,14 @@ describe('HttpRoute', function(){
       route = $httpRouteService('LoginController','index', matcher);
     }));
 
-    it('should match a path and a method', function(){
+    it('should match a path and a method and return its controller an action', function(){
       expect(route.match('/login', 'GET')).toBeTruthy();
       expect(route.match('/login', 'GET').controller).toBe('LoginController');
       expect(route.match('/login', 'GET').action).toBe('index');
+    });
+
+    it('should return false if it cannot return a path', function(){
+      expect(route.match('/xlogin', 'GET')).toBeFalsy();
     });
   });
 });
