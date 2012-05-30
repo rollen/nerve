1678ac1ac8138148de58c97b14cc92d140ffda0c
Rollen Gomes <avelliance@gmail.com>
2012-05-30 23:40:24 +0800

    fix node/npm version

---
+++ sampleapp/app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var nerve = require('./nervecenter.js').nerve;
+
+var port = process.env.PORT || 8888;
+
+http.createServer(function onRequestReceived(request, response){
+  var injector = nerve(request, response, require('fs')).
+    bootstrap();
+  
+  injector.invoke(function($server){
+    $server.run();
+  });
+
+}).listen(port);
+
+console.log('Listening on port ' + port);
+
+
