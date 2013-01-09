716d12f7d767a6f038192616ca05551653def73d
Rollen <rollen_gomes@yahoo.com>
2013-01-09 23:09:16 +0800

    refactor template.spec.js to remove dummy appconfig

--- tests/framework/application/services/template.spec.js
+++ tests/framework/application/services/template.spec.js
@@ -35,28 +35,14 @@ describe('Service.template', function(){
 	context('has its directory set' , function(){
 		it('should reads a file from the template directory', function(){
 			var fileInfoService,
-			httpFileResponseWriter;
-			
-			var appConfig = function appConfig(){
-				var object = {};
-				var context = {};
+			httpFileResponseWriter,
+			onFileReadComplete,
+			fileInfoValue,
+			args;
 
-				object.set = function(key, value){
-					context[key] = value;
-				}
-
-				object.$get = function(){
-					return function(key){
-						return context[key];
-					}
-				}
-				return object;
-			}
-
-			var onFileReadComplete = jasmine.createSpy('onFileReadComplete');
+			onFileReadComplete = jasmine.createSpy('onFileReadComplete');
 
 			injector(function($injector){
-				$injector.factory(appConfig);
 				$injector.config(function($appConfig){
 					$appConfig.set('templatefolder_path', fixturesFolderPath + '/fake');
 				});
@@ -72,9 +58,10 @@ describe('Service.template', function(){
 
 			template('index.html', onFileReadComplete);
 
-			var fileInfoValue = fileInfoService(fixturesFolderPath + '/fake', 'index.html');
+			fileInfoValue = fileInfoService(fixturesFolderPath + '/fake', 'index.html');
 
-			var args = httpFileResponseWriter.writeToResponse.mostRecentCall.args;
+			// Equality operator does not work with my object defintion
+			args = httpFileResponseWriter.writeToResponse.mostRecentCall.args;
 			expect(args[0].filename).toBe(fileInfoValue.filename);
 			expect(args[0].filepath).toBe(fileInfoValue.filepath);
 		});
