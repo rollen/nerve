e64b272359af6a8dd94391597e18ef3ca4355d1d
rollen <user@myserver.com>
2013-01-15 21:19:39 +0800

    remove unused assetcontroller test

--- tests/framework/application/controllers/assets_controller.spec.js
+++ tests/framework/application/controllers/assets_controller.spec.js
@@ -32,45 +32,4 @@ describe('AssetsController', function(){
 			expect(asset).toHaveBeenCalledWith(expected_filepath, jasmine.any(Function));
 		});
 	});
-
-  xdescribe('show', function(){
-    beforeEach(function(){
-
-      fileInfo = function(){};
-
-      injector(function($injector){
-        $injector.config(function($request, $folderpath, $fileInfo){
-          spyOn($folderpath.$getfilepath(), 'existsSync').andReturn(true);
-          $folderpath.$folder('views', '/app/client/js');
-          spyOn($request, '$get').andReturn({url:'/lib/assets/js/file.js'});
-          spyOn($fileInfo, '$get').andReturn(fileInfo);
-        });
-      });
-
-      inject(function($assetsControllerService
-                      ,$request
-                      ,$httpFileResponseWriter
-                      ,$folderpath
-                      ,$assetUrlInfoService
-                      ,$fileInfoService
-                     ){
-        assetsController = $assetsControllerService($request,
-                                                   $httpFileResponseWriter,
-                                                   $folderpath,
-                                                   $assetUrlInfoService,
-                                                   $fileInfoService
-                                                  );
-        httpFileResponseWriter = $httpFileResponseWriter;
-      })();
-
-        spyOn(httpFileResponseWriter, 'writeToResponseAndEnd');
-
-    });
-
-    it('should write a static file to the response', function(){
-      assetsController.show();
-      expect(httpFileResponseWriter.writeToResponseAndEnd).
-        toHaveBeenCalledWith(fileInfo);
-    });
-  });
 });
