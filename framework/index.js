6acc49f49f7616a6e834fc59fcf9def28b244228
rollen <user@myserver.com>
2013-01-19 13:06:58 +0800

    commit deletion of nervecenter file

--- framework/index.js
+++
@@ -1,6 +0,0 @@
-exports.Application = require('./application')
-exports.Server = require('./server')
-exports.Config = require('./config')
-exports.Patches = require('./patches')
-exports.Auto = require('./auto')
-
